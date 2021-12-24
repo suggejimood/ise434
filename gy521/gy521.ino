50ed67890d1c02cd9383d4ccb88ee73a1f0c84af
Mehmet TAN <43840863+suggejimood@users.noreply.github.com>
2021-12-24 22:49:38 +0300

    I2C
    Ekrana açı yazdırma

--- gy521/gy521.ino
+++ gy521/gy521.ino
@@ -1,9 +1,48 @@
 #include<Wire.h>
-const int MPU=0x68; 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3f,16,2);
+
+const int MPU=0x68;
+int led = 8; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-int16_t RcX, RcY, RcZ, vector, angle;
+int16_t RcX, RcY, RcZ, vector, angle, largestVector;
 
 void setup(){
+  lcd.begin();
+  pinMode(led, OUTPUT);
+
+  for(int i=0; i<3; i++){
+    for(int i = 0; i < 4; i++){
+    if(i==0)
+    {
+      lcd.setCursor(0,0); // İkinci satırın başlangıç noktası
+      lcd.print("Test Machine");
+      lcd.setCursor(0,1); // İkinci satırın başlangıç noktası
+      lcd.print("Loading   ");
+    }
+    else if(i==1){
+      lcd.setCursor(0,0); // İkinci satırın başlangıç noktası
+      lcd.print("Test Machine");
+      lcd.setCursor(0,1); // İkinci satırın başlangıç noktası
+      lcd.print("Loading.  ");
+    }
+    else if(i==2){
+      lcd.setCursor(0,0); // İkinci satırın başlangıç noktası
+      lcd.print("Test Machine");
+      lcd.setCursor(0,1); // İkinci satırın başlangıç noktası
+      lcd.print("Loading.. ");
+    }
+    else if(i==3){
+      lcd.setCursor(0,0); // İkinci satırın başlangıç noktası
+      lcd.print("Test Machine");
+      lcd.setCursor(0,1); // İkinci satırın başlangıç noktası
+      lcd.print("Loading...");
+    }
+    delay(500);
+  }
+  }
+  
   Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B); 
@@ -30,7 +69,12 @@ void loop(){
 
   vector = RcX + RcY + RcZ;
   vector = sqrt(vector);
-
+  
+  lcd.setCursor(0,1); // İkinci satırın başlangıç noktası
+  lcd.print("Vector: ");
+  lcd.setCursor(7,1); // İkinci satırın başlangıç noktası
+  lcd.print(vector);
+  
   Serial.print("Vector: "); Serial.println(vector);
   Serial.println("---------------------------");
 
@@ -38,9 +82,17 @@ void loop(){
   angle = map(AcX, -16300, 16300, 0, 180);
 
   Serial.println(angle);
+
+  lcd.setCursor(0,0); // İlk satırın başlangıç noktası
+  lcd.print("Angle: ");
+  lcd.setCursor(6,0); // İkinci satırın başlangıç noktası
+  lcd.print(angle);
   
-  if(angle == 140){
-    Serial.println("Bingoo");
+  if(angle < 145 && angle > 135){
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW);
   }
   Serial.println("-------------------");
   
@@ -50,4 +102,6 @@ void loop(){
   Serial.print(" | Z = "); Serial.println(AcZ);
   Serial.println("---------------------");
   delay(1000);
+
+  lcd.clear();
 }
